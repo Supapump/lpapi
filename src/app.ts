c13f113b03d9592f2b9d524fda15abb50dcca8c8
cutupdev <takasi75926@gmail.com>
2025-04-24 21:03:00 +0900

    create amm pool

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+import {
+  corsOptions,
+  helmetConfig,
+  limiter,
+  requestLogger,
+} from "./middleware";
+import { swaggerDocs } from "./config";
+
+import ammRouter from "./route/amm.route";
+import tokenRouter from "./route/token.route";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.use(helmetConfig);
+
+app.use(requestLogger);
+app.use(limiter);
+
+swaggerDocs(app);
+
+app.use("/api/v1/amm", ammRouter);
+app.use("/api/v1/token", tokenRouter);
+
+app.get("/api", (_req: Request, res: Response) => {
+  res.json({ message: "Server is running" });
+});
+
+export default app;
