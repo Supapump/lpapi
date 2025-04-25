7274e69a541e7cf2d442c9c8f557f718ba3459c3
icedoutskay <somkeneodikpo@gmail.com>
2025-04-25 04:41:50 +0100

    port changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const authMiddleware = require('./middleware/auth')
 const lpRoutes = require('./routes/lp')
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 
 app.use(bodyParser.json());
