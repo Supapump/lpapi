43dab599cc663dcea6095e165f57b6065f05b046
icedoutskay <somkeneodikpo@gmail.com>
2025-04-25 07:29:17 +0100

    Fix port binding

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+require('dotenv').config(); 
+
 const app = express();
 const authMiddleware = require('./middleware/auth');
 const lpRoutes = require('./routes/lp');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  
 
 app.use(bodyParser.json());
 app.use('/api/lp', lpRoutes);
 
-const server = () => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-};
-
-
-server();
+app.listen(PORT, () => {
+    console.log(`Server is successfully listening on port ${PORT}`);
+}).on('error', (err) => {
+    console.error('Error starting the server:', err);
+});
